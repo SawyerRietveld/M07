81e3abbe8f487c0b3d67ba27fd70eba6dfb612e7
Sawyer Rietveld <srietveld@ivytech.edu>
2024-12-06 19:31:41 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+
+const app = express();
+
+const dbURI = "mongodb+srv://Ninja:TEST1234@srsdev255m06.e49gc.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=SRSDEV255M06";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+app.use('/blogs', blogRoutes);
+
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
